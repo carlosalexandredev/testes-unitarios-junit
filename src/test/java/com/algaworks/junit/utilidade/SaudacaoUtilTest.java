9961ec1867d74f8231cded98513cd36a03af931f
carlosalexandredev <carlosalexandre5670@gmail.com>
2023-02-11 08:59:42 -0300

    Organizando testes com o padrão Tripe A

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,13 +1,16 @@
 package com.algaworks.junit.utilidade;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static com.algaworks.junit.utilidade.SaudacaoUtil.saudar;
 import static com.algaworks.junit.utilidade.SaudacaoUtilConditions.igualBomDia;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SaudacaoUtilTest {
 
@@ -19,7 +22,7 @@ class SaudacaoUtilTest {
     @Test
     public void saudarBomdia() {
         for (int hora : TipoSaudacao.BOM_DIA.getHoras()) {
-            String saudacao = SaudacaoUtil.saudar(hora);
+            String saudacao = saudar(hora);
             assertThat(saudacao).is(igualBomDia());
         }
     }
@@ -27,7 +30,7 @@ class SaudacaoUtilTest {
     @Test
     public void saudarBoaTarde() {
         for (int hora : TipoSaudacao.BOA_TARDE.getHoras()) {
-            String saudacao = SaudacaoUtil.saudar(hora);
+            String saudacao = saudar(hora);
             assertEquals("Boa tarde", saudacao, String.format("Saudacao incorreta | HORA: %s", hora));
         }
     }
@@ -35,7 +38,7 @@ class SaudacaoUtilTest {
     @Test
     public void saudarBoaNoite() {
         for (int hora : TipoSaudacao.BOA_NOITE.getHoras()) {
-            String saudacao = SaudacaoUtil.saudar(hora);
+            String saudacao = saudar(hora);
             assertEquals("Boa noite", saudacao, String.format("Saudacao incorreta | HORA: %s", hora));
         }
     }
@@ -43,10 +46,7 @@ class SaudacaoUtilTest {
     @Test
     public void deveLancarException() {
         int horaInvalida = -10;
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> SaudacaoUtil.saudar(horaInvalida));
-        assertEquals("Hora inválida", illegalArgumentException.getMessage());
-
-        Assertions.assertThatThrownBy(() -> SaudacaoUtil.saudar(horaInvalida))
+        assertThatThrownBy(() -> saudar(horaInvalida))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Hora inválida");
     }
@@ -57,13 +57,15 @@ class SaudacaoUtilTest {
      */
     @Test
     public void naoDeveLancarException() {
-        assertDoesNotThrow(() -> SaudacaoUtil.saudar(0));
+        int horaValida = 0;
+        Executable chamadaValidaDeMetodo = () -> saudar(horaValida);
+        assertDoesNotThrow(chamadaValidaDeMetodo);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {5, 6, 7, 8, 9, 10, 11})
     public void Dado_horario_matinal_quando_saudar_entao_deve_retornar_bom_dia(int hora) {
-        String saudacao = SaudacaoUtil.saudar(hora);
+        String saudacao = saudar(hora);
         assertEquals(TipoSaudacao.BOM_DIA.getDescricao(), saudacao, String.format("Saudacao incorreta | HORA: %s", hora));
     }
 }
