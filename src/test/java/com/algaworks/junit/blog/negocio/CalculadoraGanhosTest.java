44e75f8d602088ef49b3294e23b944ceeee241a0
carlosalexandredev <carlosalexandre5670@gmail.com>
2023-02-13 16:16:29 -0300

    Refatoração dos Testes de CalculadoraGanhos

--- src/test/java/com/algaworks/junit/blog/negocio/CalculadoraGanhosTest.java
+++ src/test/java/com/algaworks/junit/blog/negocio/CalculadoraGanhosTest.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class CalculadoraGanhosTest {
 
     static CalculadoraGanhos calculadora;
@@ -31,23 +32,33 @@ class CalculadoraGanhosTest {
 
 
     @Test
-    public void deveCalcularGanhos() {
+    public void Dado_post_e_autor_premio_Quando_calcular_ganhos_Entao_deve_retornar_valor_com_bonus() {
         Ganhos ganhos = calculadora.calcular(post);
 
         assertEquals(new BigDecimal(45), ganhos.getTotalGanho());
-        assertEquals(7, ganhos.getQuantidadePalavras());
-        assertEquals(autor.getValorPagoPorPalavra(), ganhos.getValorPagoPorPalavra());
     }
 
     @Test
-    public void deveCalcularGanhosSemPremium() {
-        autor.setPremium(false);
+    public void Dado_post_e_autor_premio_Quando_calcular_ganhos_Entao_deve_retornar_quantidade_palavras_no_post() {
+        Ganhos ganhos = calculadora.calcular(post);
 
+        assertEquals(7, ganhos.getQuantidadePalavras());
+    }
+
+    @Test
+    public void Dado_post_e_autor_premio_Quando_calcular_ganhos_Entao_deve_retonar_valor_pago_por_palavra_do_autor() {
+        Ganhos ganhos = calculadora.calcular(post);
+
+        assertEquals(autor.getValorPagoPorPalavra(), ganhos.getValorPagoPorPalavra());
+    }
+
+
+    @Test
+    public void Dado_post_e_autor_nao_premio_Quando_calcular_ganhos_Entao_deve_retornar_valor_com_bonus() {
+        autor.setPremium(false);
         Ganhos ganhos = calculadora.calcular(post);
 
         assertEquals(new BigDecimal(35), ganhos.getTotalGanho());
-        assertEquals(7, ganhos.getQuantidadePalavras());
-        assertEquals(autor.getValorPagoPorPalavra(), ganhos.getValorPagoPorPalavra());
     }
 
 }
