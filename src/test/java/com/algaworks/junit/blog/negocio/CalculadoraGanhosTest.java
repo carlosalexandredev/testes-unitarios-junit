c0b5da4698022d493291227355fc054fc8e0e458
carlosalexandredev <carlosalexandre5670@gmail.com>
2023-02-13 15:52:02 -0300

    Preparando o cenário de testes com @BeforeEach e @BeforeAll

---
+++ src/test/java/com/algaworks/junit/blog/negocio/CalculadoraGanhosTest.java
@@ -0,0 +1,53 @@
+package com.algaworks.junit.blog.negocio;
+
+import com.algaworks.junit.blog.modelo.Editor;
+import com.algaworks.junit.blog.modelo.Ganhos;
+import com.algaworks.junit.blog.modelo.Post;
+import com.algaworks.junit.blog.utilidade.ProcessadorTextoSimples;
+import org.junit.jupiter.api.*;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraGanhosTest {
+
+    static CalculadoraGanhos calculadora;
+    Editor autor;
+
+    Post post;
+
+    @BeforeAll
+    static void initAll() {
+        calculadora = new CalculadoraGanhos(new ProcessadorTextoSimples(), BigDecimal.TEN);
+    }
+
+    @BeforeEach
+    void init() {
+        autor = new Editor(1L, "Alex", "alex@email.com", new BigDecimal(5), true);
+        post = new Post(1L, "Ecossitema Java", "O ecossistema do Java, é muito maduro...", autor,
+                "ecossistema-java-abc123", null, false, false);
+    }
+
+
+    @Test
+    public void deveCalcularGanhos() {
+        Ganhos ganhos = calculadora.calcular(post);
+
+        assertEquals(new BigDecimal(45), ganhos.getTotalGanho());
+        assertEquals(7, ganhos.getQuantidadePalavras());
+        assertEquals(autor.getValorPagoPorPalavra(), ganhos.getValorPagoPorPalavra());
+    }
+
+    @Test
+    public void deveCalcularGanhosSemPremium() {
+        autor.setPremium(false);
+
+        Ganhos ganhos = calculadora.calcular(post);
+
+        assertEquals(new BigDecimal(35), ganhos.getTotalGanho());
+        assertEquals(7, ganhos.getQuantidadePalavras());
+        assertEquals(autor.getValorPagoPorPalavra(), ganhos.getValorPagoPorPalavra());
+    }
+
+}
