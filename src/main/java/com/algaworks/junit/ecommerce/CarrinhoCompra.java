1992610dab0300aff7e1408bbf57390263d54a66
carlosalexandredev <carlosalexandre5670@gmail.com>
2023-02-14 10:48:39 -0300

    Implementação de testes para método de adicionar produto ao carrinho de compras.

--- src/main/java/com/algaworks/junit/ecommerce/CarrinhoCompra.java
+++ src/main/java/com/algaworks/junit/ecommerce/CarrinhoCompra.java
@@ -38,20 +38,24 @@ public class CarrinhoCompra {
 	// Quantidade não pode ser menor que 1
 	// Deve incrementar a quantidade caso o produto já exista
 	public void adicionarProduto(Produto produto, int quantidade) {
-		verificaParametrosNulos(produto);
+		verificaProdutoNulo(produto);
 		if (quantidade < 1)
 			throw new RegraNegocioException("Quantidade não pode ser menor que 1");
 
-		itens.stream()
-				.filter(prod -> prod.equals(produto))
-				.forEach(prod -> prod.adicionarQuantidade(1));
+		Optional<ItemCarrinhoCompra> itemExistente = itens.stream().filter(item -> item.getProduto().equals(produto)).findFirst();
+
+		if (itemExistente.isPresent()) {
+			itemExistente.get().adicionarQuantidade(quantidade);
+		} else {
+			itens.add(new ItemCarrinhoCompra(produto, quantidade));
+		}
 	}
 
 	// Parâmetro não pode ser nulo, deve retornar uma exception
 	// Caso o produto não exista, deve retornar uma exception
 	// Deve remover o produto independente da quantidade
 	public void removerProduto(Produto produto) {
-		verificaParametrosNulos(produto);
+		verificaProdutoNulo(produto);
 		verificaSeProdutoExiste(produto);
 		itens.removeIf(prod -> prod.equals(produto));
 	}
@@ -61,7 +65,7 @@ public class CarrinhoCompra {
 	// Caso o produto não exista, deve retornar uma exception
 	// Deve aumentar em um quantidade do produto
 	public void aumentarQuantidadeProduto(Produto produto) {
-		verificaParametrosNulos(produto);
+		verificaProdutoNulo(produto);
 		verificaSeProdutoExiste(produto);
 
 		itens.stream()
@@ -74,7 +78,7 @@ public class CarrinhoCompra {
 	// Caso o produto não exista, deve retornar uma exception
 	// Deve diminuir em um quantidade do produto, caso tenha apenas um produto, deve remover da lista
 	public void diminuirQuantidadeProduto(Produto produto) {
-		verificaParametrosNulos(produto);
+		verificaProdutoNulo(produto);
 		verificaSeProdutoExiste(produto);
 		itens.stream().filter(prod -> prod.equals(produto))
 				.forEach(prod -> prod.subtrairQuantidade(1));
@@ -104,7 +108,7 @@ public class CarrinhoCompra {
 		return Objects.equals(itens, that.itens) && Objects.equals(cliente, that.cliente);
 	}
 
-	private static void verificaParametrosNulos(Produto produto) {
+	private static void verificaProdutoNulo(Produto produto) {
 		if (Objects.isNull(produto))
 			throw new RegraNegocioException("Parâmetros não podem ser nulos");
 	}
--- src/test/java/com/algaworks/junit/ecommerce/CarrinhoCompraTest.java
+++ src/test/java/com/algaworks/junit/ecommerce/CarrinhoCompraTest.java
@@ -1,11 +1,70 @@
 package com.algaworks.junit.ecommerce;
 
-import org.junit.jupiter.api.DisplayName;
+import com.algaworks.junit.blog.exception.RegraNegocioException;
+import org.junit.jupiter.api.*;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("Teste Carrinho de Compra")
+@DisplayName("Testes Carrinho de Compra:")
 class CarrinhoCompraTest {
+    Cliente cliente = new Cliente(1L, "Carlos Alexandre");
+    CarrinhoCompra carrinhoCompra = new CarrinhoCompra(cliente);
+
+    @Nested
+    @DisplayName("Adição de produto ao carrinho:")
+    class ProdutoCarrinho {
+
+        Produto produto;
+
+        @BeforeEach
+        void criaProduto() {
+            produto = new Produto(1L,
+                    "Computador",
+                    "Computador completo 8GB Ram, 500GB SSD",
+                    new BigDecimal(4000));
+        }
+
+        @Test
+        @DisplayName("Dado produto nulo ao adicionar no carrinho de compras, Então deve retornar uma excessão")
+        void produtoNuloDeveRetornarExcessao() {
+            RegraNegocioException exception = assertThrows(RegraNegocioException.class, () -> carrinhoCompra.adicionarProduto(null, 1));
+            assertEquals("Parâmetros não podem ser nulos", exception.getMessage());
+        }
+
+        @Test
+        @DisplayName("Dado uma quantidade de produto menor que 1 ao adicionar no carrinho de compras, Então deve laçar uma excessão")
+        void quantidadeProdutoMenorQueUmRetornarExcessao() {
+            RegraNegocioException exception = assertThrows(RegraNegocioException.class, () -> carrinhoCompra.adicionarProduto(produto, -2));
+            assertEquals("Quantidade não pode ser menor que 1", exception.getMessage());
+        }
 
 
+        @Test
+        @DisplayName("Dado um produto que não exista no carrinho de compras, Então deve adiciona-lo")
+        void produtoNaoExistenteDeveAdicionar() {
+            Produto produtoNovo = new Produto(2L, "Teclado", "Teclado mecânido com Led", new BigDecimal(200));
+            carrinhoCompra.adicionarProduto(produtoNovo, 12);
+            assertTrue(carrinhoCompra.getItens().stream().anyMatch(item -> item.getProduto().equals(produtoNovo)));
+        }
+
+        @Test
+        @DisplayName("Dado um produto que já exista no carrinho de compras, Então deve incrementar sua quantidade")
+        void produtoExistenteIncrementarQuantidade() {
+            int quantidadeA = 5;
+            int quantidadeB = 8;
+            carrinhoCompra.adicionarProduto(produto, quantidadeA);
+            carrinhoCompra.adicionarProduto(produto, quantidadeB);
+
+            int quantidadeEsperada = quantidadeA + quantidadeB;
+            int quantidadeRetornada = carrinhoCompra.getItens().stream()
+                    .filter(item -> item.getProduto().equals(produto))
+                    .mapToInt(ItemCarrinhoCompra::getQuantidade)
+                    .sum();
+
+            assertEquals(quantidadeEsperada, quantidadeRetornada);
+        }
+
+    }
 }
