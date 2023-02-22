f32f356f61d8b06c972cae2306b856fb08ee4543
carlosalexandredev <carlosalexandre5670@gmail.com>
2023-02-22 01:02:59 -0300

    Reestruturando projeto | Adicionando tdd-essencial

---
+++ tdd-essencial/camel-case-1/src/test/java/org/example/CamelCaseConverterTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Conversor CamelCase")
+public class CamelCaseConverterTest {
+
+    private CamelCaseConverter camelCase;
+
+    @BeforeEach
+    void init(){
+        camelCase = new CamelCaseConverter();
+    }
+
+    @Test
+    @DisplayName("Cria objeto CamelCase")
+    void deveCriarObjetoCamelCaseCanverter() throws Exception {
+    }
+
+    @Test
+    @DisplayName("Aplica CamelCase em nome inicial")
+    void aplicarCamelCaseEmNomeInico() throws Exception {
+        assertEquals("Lionel", camelCase.converter("lionel"));
+    }
+
+    @Test
+    @DisplayName("Converte nome simples misturado maiusculo e minusculo")
+    void deveConverterNomeSimplesMisturadoMaiusculoMinusculo(){
+        assertEquals("Lionel", camelCase.converter("LIOnel"));
+    }
+}
